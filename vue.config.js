99feba58e98645cb762903710ce7502353d3fa68
YaraZan <115586290+YaraZan@users.noreply.github.com>
2023-03-15 08:42:00 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,14 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: 'localhost',
+    disableHostCheck: true,
     port: 8080,
     hot: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
     }
   }
 })
